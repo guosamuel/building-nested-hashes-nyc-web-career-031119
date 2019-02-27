283929e815cf5e4009c259b2f03695f12eafb293
Samuel Guo <guosamuel1114@gmail.com>
2019-02-27 21:26:40 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,12 +2,12 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague"
+        {:name => "Lord Montague"}
         :age => "53"
         },
       :matriarch => {
-        :name => "Lady Montague"
-        :age => "54"
+        {:name => "Lady Montague"
+        :age => "54"}
         },
       :hero => {
         :name => "Romeo"
